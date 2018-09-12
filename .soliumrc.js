fdd3210233a3258ccfedbb58a493df9e7deaf77b
Ferit Tunçer <ferit@cryptolab.net>
2018-09-12 17:23:44 +0300

    ci(solium): require error-reason severity increase
    makes Solium to throw an error when a require statement is missing error reason argument

--- .soliumrc.js
+++ .soliumrc.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     quotes: ['error', 'double'],
     indentation: ['error', 4],
-    'security/no-block-members': ['off']
+    'security/no-block-members': ['off'],
+    'error-reason': ['error']
   }
 }
