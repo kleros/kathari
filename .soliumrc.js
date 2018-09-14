1c40dd26236ef3bab091380ebcec7d08454723f7
epiqueras <epiquerass@gmail.com>
2018-09-14 14:36:53 -0500

    feat(solium): allow send and inline assembly

--- .soliumrc.js
+++ .soliumrc.js
@@ -5,6 +5,8 @@ module.exports = {
     quotes: ['error', 'double'],
     indentation: ['error', 4],
     'error-reason': ['error'],
-    'security/no-block-members': ['off']
+    'security/no-block-members': ['off'],
+    'security/no-send': ['off'],
+    'security/no-inline-assembly': ['off']
   }
 }
